94daffb141e47c60f4fb382470de8bb023675c5a
Napchat <wozhendeaiwoa@gmail.com>
2017-06-11 23:06:26 +0800

    forms

--- app/forms.py
+++ app/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
-from wtforms.validators import DataRequired
+from wtforms import StringField, BooleanField, TextAreaField
+from wtforms.validators import DataRequired, Length
 
 class LoginForm(FlaskForm):
 	#: the DataRequired import is a validator, a function that
@@ -9,3 +9,7 @@ class LoginForm(FlaskForm):
 	#: simply checks that the field is not submitted empty.
 	openid = StringField('openid', validators=[DataRequired()])
 	remember_me = BooleanField('remember_me', default=False)
+
+class EditForm(FlaskForm):
+	nickname = StringField('nickname', validators=[DataRequired()])
+	about_me = TextAreaField('about_me', validators=[Length(min=0, max=140)])
