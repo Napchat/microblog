ec478924e6158a682602b3ff12cc835532f98253
Napchat <wozhendeaiwoa@gmail.com>
2017-06-13 12:19:06 +0800

    add mail server

--- config.py
+++ config.py
@@ -22,3 +22,12 @@ SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
 
 #: the folder where we will store our migrate data files
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+
+#: mail server settings
+MAIL_SERVER = 'localhost'
+MAIL_PORT = 25
+MAIL_USERNAME = None
+MAIL_PASSWORD = None
+
+#: administrator list
+ADMINS = ['you@example.com']
